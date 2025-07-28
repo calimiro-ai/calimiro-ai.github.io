407617670fb2b27e33ba33d758bf7f022dcf5262
ixodev <ixodev0@gmail.com>
2025-07-28 19:13:40 +0200

    gallery some pics

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,21 @@
+var galleryImageIndex = 0;
+const numImages = 0;
+
 function goToGallery() {
-    
+    window.location.href = '/gallery.html';
+    galleryImageIndex = 0;
 }
 
 function goToDocumentation() {
     window.location.href = 'https://github.com/calimiro-ai';
+}
+
+function goToNextImage() {
+    galleryImageIndex++;
+
+    if(galleryImageIndex > numImages) {
+        galleryImageIndex = 0;
+    }
+
+    document.getElementById("gallery-image-displayed").src = "images/gallery/gallery_" + galleryImageIndex + ".jpg";
 }
